f9c310f28685cde4ffb57360cbb6ffcfe161e4a5
xyfgh <953182870@qq.com>
2016-07-29 11:53:02 +0800

    创建仓库

---
+++ js/js.js
@@ -0,0 +1,11 @@
+function show(id) {
+	var element = document.getElementById(id);
+	// var classname = element.className;
+	element.className = 'shopClassListPop ' + 'show';
+}
+function hide(id) {
+	var element = document.getElementById(id);
+	// var classname = element.className;
+	element.className = 'shopClassListPop ' + 'hide';
+}
+
